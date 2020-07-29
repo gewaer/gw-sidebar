d758671436ff817f5a79e4e427034fe3a224794f
freesgen <jesusant.guerrero@gmail.com>
2020-07-29 11:42:20 -0400

    Fix identation

--- src/item-group.vue
+++ src/item-group.vue
@@ -1,87 +1,87 @@
 <template>
-  <div :class="{ 'open-category-menu' : isActive}" class="menu-category row flex-column no-gutters">
-    <span
-        :class="{ active: isActive }"
-        class="menu-category-title py-3 px-4 d-flex justify-content-between align-items-center" 
-        @click.prevent="toggleActive()"
-    >
+    <div :class="{ 'open-category-menu' : isActive}" class="menu-category row flex-column no-gutters">
+        <span
+            :class="{ active: isActive }"
+            class="menu-category-title py-3 px-4 d-flex justify-content-between align-items-center" 
+            @click.prevent="toggleActive()"
+        >
             {{ label }}
             <i class="fas" :class="`${arrowIcon}`" />
-    </span>
+        </span>
 
-    <div class="menu-category-links">
-        <template v-for="(resource, index) in childs">
-            <side-item
-                v-if="!resource[childName]|| resource[childName].length"
-                :key="`resource-${label}-${index}`"
-                :resource="resource"
-            />
-        </template>
+        <div class="menu-category-links">
+            <template v-for="(resource, index) in childs">
+                <side-item
+                    v-if="!resource[childName]|| resource[childName].length"
+                    :key="`resource-${label}-${index}`"
+                    :resource="resource"
+                />
+            </template>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 import SideItem from "./item";
 
 export default {
-  props: {
-      label: {
-          type: String,
-          required: true
-      },
-      childs: {
-        type: Array,
-        default() {
-          return []
+    props: {
+        label: {
+            type: String,
+            required: true
+        },
+        childs: {
+            type: Array,
+            default() {
+            return []
+            }
+        },
+        current: {
+            type: String,
+            required: true
+        },
+        menuId: {
+            type: String,
+            required: true
+        },
+        childName: {
+            type: String, 
+            default: "links"
         }
-      },
-      current: {
-        type: String,
-        required: true
-      },
-      menuId: {
-        type: String,
-        required: true
-      },
-      childName: {
-        type: String, 
-        default: "links"
-      }
-  },
-  components: {
-    SideItem
-  },
-  filters: {
-      firstLetter(value) {
-          return value.charAt(0);
-      }
-  },
-  computed: {
-    arrowIcon() {
-      return "fa-chevron-right";
     },
-    isActive() {
-      return this.current == this.menuId;
+    components: {
+        SideItem
+    },
+    filters: {
+        firstLetter(value) {
+            return value.charAt(0);
+        }
+    },
+    computed: {
+        arrowIcon() {
+            return "fa-chevron-right";
+        },
+        isActive() {
+            return this.current == this.menuId;
+        }
+    },
+    methods: {
+        toggleActive() {
+            this.$emit("toggle-active", this.menuId);
+        }
     }
-  },
-  methods: {
-    toggleActive() {
-      this.$emit("toggle-active", this.menuId);
-    }
-  }
 };
 </script>
 
 <style lang="scss" scoped>
 .menu-item-group {
-  cursor: pointer;
-  border-top: 1px solid  var(--base-color);
-  color: var(--base-color);
-  
-  .toggle, .icon-thumbnail i {
-    color: var(--base-color) !important;
-  }
+    cursor: pointer;
+    border-top: 1px solid  var(--base-color);
+    color: var(--base-color);
+    
+    .toggle, .icon-thumbnail i {
+        color: var(--base-color) !important;
+    }
 }
 </style>
 
